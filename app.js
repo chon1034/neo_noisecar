fa456d00c5c5418ffd5d7eb14490b510ab668da8
chon1034 <chon1034@gmail.com>
2025-02-25 22:06:21 +0800

    Create index.html

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, Neo Noisecar!');
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
